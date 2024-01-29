81e407e9748415d32e329b560fd440926169e10e
SseryG <emanaev2000@mail.ru>
2024-01-29 15:49:15 +0700

    change file.py

--- file.py
+++ file.py
@@ -1,15 +1,8 @@
 from read import ReadPdf,ReadDoc,ReadDocx
 import pandas as pd
 from IPython.display import display
-<<<<<<< HEAD
 from clusters import TFidfVector,OneCluster,TwoCluster
-=======
-<<<<<<< HEAD
-from clusters import TFidfVector,Cluster
-=======
-from clusters import CountVector,token,TFidf,TFidfVector,Cluster
->>>>>>> 26b0df96911c86a4753907f449ddbbd36239bbdf
->>>>>>> 52f278f2ec19434b5f812d54137555a5599c3cb2
+
 import os
 import asyncio
 import time
@@ -19,7 +12,7 @@ class FileClass(object):
         self.clustersfile = []
         self.listfiles= []
 
-<<<<<<< HEAD
+
     def AbsolutePath(self,path):#обход по папкам
         tic = time.perf_counter()
         self.listfiles=WalkDir(path)
@@ -60,10 +53,8 @@ def Data(file,p):#прочтение документов
         print(file)  
         return ReadDoc(file)#
 
-def Exeption(p):
-    if(p[1]=='.pdf'or p[1]=='.docx'or p[1]=='.doc'):   
-=======
-<<<<<<< HEAD
+
+
 def Data(file,p):#прочтение документов
     if (p[1]=='.pdf'):
         print(file)  
@@ -77,48 +68,18 @@ def Data(file,p):#прочтение документов
 
 def Exeption(p):
     if(p[1]=='.pdf'or p[1]=='.docx'or p[1]=='.doc'):   
-=======
-
-
-def Data(dirs,file,p,number):#прочтение документов
-    if (p[1]=='.pdf'):
-        print('{}{}'.format(number,file))
-        return ReadPdf(os.path.join(dirs,file))#
-    elif (p[1]=='.docx'):
-        print('{}{}'.format(number,file))
-        return ReadDocx(os.path.join(dirs,file))#
-    elif (p[1]=='.doc'):        
-        print('{}{}'.format(number,file))        
-        return ReadDoc(os.path.join(dirs,file))#
-
-def Exeption(p):
-    if(p[1]=='.pdf'or p[1]=='.docx'or p[1]=='.doc'):
->>>>>>> 26b0df96911c86a4753907f449ddbbd36239bbdf
->>>>>>> 52f278f2ec19434b5f812d54137555a5599c3cb2
         return True
     else:
         return False
-    
-<<<<<<< HEAD
+
+
 def WalkDir(path):
     listfiles=[]
-=======
-<<<<<<< HEAD
-def WalkDir(path):
-    listfiles=[]
-=======
-def WalkDir(path,listfiles):
->>>>>>> 26b0df96911c86a4753907f449ddbbd36239bbdf
->>>>>>> 52f278f2ec19434b5f812d54137555a5599c3cb2
     for dirs,folden,files in os.walk(path):
         for file in files:          
             if Exeption(os.path.splitext(file)):
                 listfiles.append(os.path.join(dirs,file))
     return  listfiles
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 52f278f2ec19434b5f812d54137555a5599c3cb2
 
 def ChekingSize(listfile):
     smallfile=[]
@@ -147,15 +108,7 @@ def ChekingNone(List,predfile,file):
     else :
         List.remove(file)
         return False
-<<<<<<< HEAD
-    
-=======
 
-=======
-    
-def IsNone(listfiles):
-    for i in listfiles:
-        if i!=None
            
 
 def WalkListFile(listfiles):
@@ -164,64 +117,12 @@ def WalkListFile(listfiles):
         if os.stat(i).st_size>52428800:
             smallfile.append(i)
     return smallfile
->>>>>>> 26b0df96911c86a4753907f449ddbbd36239bbdf
     
-def AbsolutePath(path):#обход по папкам
-    number=1
-    tic = time.perf_counter()
-<<<<<<< HEAD
-    listfiles=WalkDir(path)
-    smallfile,bigfile=ChekingSize(listfiles)    
-    artext=WalkList(smallfile)
-    artext1=WalkList(bigfile)
-    bigvector=TFidfVector(artext1)
-    indexfile.append(smallfile)
-    indexfile.append(bigfile)
-    clustersfile=Cluster(TFidfVector(artext),bigvector)
-=======
-    listfiles=WalkDir(path,listfiles)
-    bigfile=[]    
-    smallfile=[]   
-      
-            if os.stat(os.path.join(dirs,file)).st_size>5242880:
-               os.path.join(dirs,file)
-            else:           
-                p=os.path.splitext(file)
-                predfile=Data(dirs,file,p,number)
-                if ( predfile is not None):               
-                    smallfile.append(predfile)
-                    pathfile=os.path.join(dirs,file)
-                    listfiles.append(pathfile)
-                    number+=1
-                    predfile=None
->>>>>>> 26b0df96911c86a4753907f449ddbbd36239bbdf
     
-    Cluster(TFidfVector(smallfile))
-    #print(klast)
-    
-    toc = time.perf_counter()
-    print(f"Вычисление заняло {toc - tic:0.4f} секунд")
+
+
 
     
     
-def CreateDirectory(twopath):
-    twopath=os.path.join(twopath,'dir')
-    os.mkdir(twopath)
-<<<<<<< HEAD
-    for cluster in clustersfile:
-        pathcluster=twopath+"\\cluster_%i"%cluster     
-        if(not os.path.isdir(pathcluster)):  
-            os.mkdir(pathcluster)        
-        for file in indexfile:
-            os.rename(file,pathcluster)
 
-
-=======
->>>>>>> 26b0df96911c86a4753907f449ddbbd36239bbdf
-    
-
->>>>>>> 52f278f2ec19434b5f812d54137555a5599c3cb2
-#path='F:/'
-#twopath='C:/Users/Сергей/Desktop/reader/dir/dir'
-#AbsolutePath(twopath)
 
