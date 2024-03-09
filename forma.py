205569c61d092e5865e18b1143fc56720d95dd4f
SseryG <emanaev2000@mail.ru>
2024-03-10 01:35:41 +0700

    tkinter

--- forma.py
+++ forma.py
@@ -1,11 +1,121 @@
 import sys
 import os
+from tkinter import *
+
+import tkinter.filedialog as fd
 from PyQt5.QtGui     import *
 from PyQt5.QtCore    import *
 from PyQt5.QtWidgets import *
 from file import FileClass
+#import errorbox
+import threading
 
 
+class App(Tk):
+   def __init__(self):  
+      self.file=FileClass()
+      super().__init__()        
+      self.title('SD')
+      self.minsize(319,349)
+      self.maxsize(321,351)
+      self.geometry('320x350')  
+      self.labelinputpath=Label(self,font='Arial 10',text="Папка с документами")
+      self.labelinputpath.place(x = 40,y = 70)
+
+      self.entryinputpath=Entry(self,font='Arial 10')
+      self.entryinputpath.place(x = 40,y = 90,width=200,height=15)
+      self.entryinputpath.insert(0,"C:/Dekstop/dir")
+
+      self.photodelete=PhotoImage(file="icon/icondelete.png")
+      self.photodir=PhotoImage(file="icon/icondir.png")
+      self.deleteinputbtn=Button(
+         self,
+         image =self.photodelete,
+         command=self.ClicDelEntryInput
+      )
+      self.deleteinputbtn.place(x=245,y=90,width=15,height=15)
+
+      self.diropeninputbtn=Button(
+         self,
+         image=self.photodir,
+         command=self.ClicOpenDirInpat
+      )
+      self.diropeninputbtn.place(x=260,y=90,width=15,height=15)
+
+      self.labeltransferpath=Label(self,font='Arial 10',text="Папка перемещения документов")
+      self.labeltransferpath.place(x = 40,y = 120)
+
+      self.entrytransferpath=Entry(self,font='Arial 10')
+      self.entrytransferpath.place(x = 40,y = 140,width=200,height=15)
+      self.entrytransferpath.insert(0,"C:/Dekstop/dir")
+
+      self.deletetransferbtn=Button(
+         self,
+         image =self.photodelete,
+         activeforeground = "Gray",
+         command=self.ClicDelEntryTransfer
+      )
+      self.deletetransferbtn.place(x=245,y=140,width=15,height=15) 
+
+      self.diropentransferbtn=Button(
+         self,
+         image=self.photodir,
+         command=self.ClicOpenDirTransfer
+      )
+      self.diropentransferbtn.place(x=260,y=140,width=15,height=15)
+
+      self.pusk=Button(
+         self,
+         text="Проход по папкам",
+         command=threading.Thread(target=self.ClicPusk).start
+      )
+      self.pusk.place(x=40,y=200,width=235,height=20)
+
+      self.createfile=Button(
+         self,
+         text="Создание папок",
+         command=threading.Thread(target=self.ClicCreate).start
+      )
+      self.createfile.place(x=40,y=240,width=235,height=20)
+         
+   def ClicDelEntryInput(self):
+      self.entryinputpath.delete(0,END)
+
+   def ClicDelEntryTransfer(self):
+      self.entrytransferpath.delete(0,END)
+
+   def ClicOpenDirInpat(self):
+      self.directory = fd.askdirectory(title="Выбрать папку", initialdir="/")
+      self.entryinputpath.delete(0,END)
+      self.entryinputpath.insert(0,self.directory)
+
+   def ClicOpenDirTransfer(self):
+      self.directory = fd.askdirectory(title="Выбрать папку", initialdir="/")
+      self.entrytransferpath.delete(0,END)
+      self.entrytransferpath.insert(0,self.directory)
+
+   def ClicPusk(self):
+      path=self.entryinputpath.get()
+     # try:         
+      self.file.AbsolutePath(path)
+      #except:         
+         #errorbox.InputEntry(path)
+
+
+   def ClicCreate(self):
+      path=self.entrytransferpath.get()
+      #try:         
+      self.file.CreateDirectory(path)
+      #except:
+         #errorbox.TransferEntry(path)
+
+if __name__=="__main__":
+   app=App()
+   app.mainloop()
+
+
+
+'''
 class window(QWidget):   
    def __init__(self, parent = None):
       self.file=FileClass()
@@ -101,3 +211,4 @@ if __name__ == '__main__':
    ex = window()
    ex.show()
    sys.exit(app.exec_())
+'''
