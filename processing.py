b3cea99847fb1af9df60771b245bb028f9d55878
SseryG <emanaev2000@mail.ru>
2024-03-10 01:46:44 +0700

    del function word

--- processing.py
+++ processing.py
@@ -5,14 +5,14 @@ import nltk
 #nltk.download('perluniprops')
 #nltk.download('universal_tagset')
 #nltk.download('stopwords')
-#nltk.download('nonbreaking_prefixes')
+#nltk.download('nonbreaking_prefixes')s
 #nltk.download('wordnet')
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from nltk.stem import SnowballStemmer
 import re
 from pymorphy3 import MorphAnalyzer
-
+#nltk.download('averaged_perceptron_tagger_ru')
 import time
 
 
@@ -23,47 +23,55 @@ stopw.extend(['сами','таких','иной','также','этим','эти
 stopw.extend(['любой','например','пример','наша','которого','глава','этими','каждого','ними','такую','которой', 'свой','такое'])
 stopw.extend(['которые','рисунок','таблица','которых','котором','какие','каком','какого','этому','либо','запятая', 'многом'])
 stopw.extend(['одной','должны','однако','могли','очень','нужно','одного','должно','вашего','можете','разных','такие','вашем'])
-stopw.extend(['стало','иначе','каждом','таким','каждый','такая','затем','','','','','',''])
+stopw.extend(['стало','иначе','каждом','таким','каждый','такая','затем','причем','несколько','будем','будут','важными','важно'])
+stopw.extend(['важных','одним','любом','двух','число','вторая','второго','первый','каждое','разной','','','','','',''])
 
-def Steemm(text,stemmer=SnowballStemmer('russian')):#выделаение основы
-    return [stemmer.stem(s) for s in text]
+def Steemm(text,stemmer=SnowballStemmer('russian')):#выделаение основы    
+    return  [stemmer.stem(s) for s in text]
 
 def LemmatizeMorphWord(token,morph=MorphAnalyzer()):
-    return morph.parse(token)[0].normal_form
+    return  morph.parse(token)[0].normal_form
 
 def LemmatizeMorphText(text):#выделение схожих слов
-    return [LemmatizeMorphWord(w) for w in text]
+    return  [LemmatizeMorphWord(w) for w in text]
 
 
 def SumRe(text):#удаление символов и понижение регистров
-    text=re.sub(r'[0123456789+]','',text)
-    text=re.sub('-\n','',text)
-    text=re.sub('-','',text)
-    text=re.sub('\n',' ',text)
-    text=text.lower()
-    text=regex.findall(text)
+    text= re.sub(r'[0123456789+]','',text)
+    text= re.sub('-\n','',text)
+    text= re.sub('-','',text)
+    text= re.sub('\n',' ',text)
+    text= text.lower()
+    text= regex.findall(text)
     return text
 
-
-def Token(text):#выделение токенов
-    #print(stopw)
-    
+def Token(text):
     if (text is not None):
         tic = time.perf_counter()
-        text=SumRe(text)
+           
+        text=' '.join(SumRe(text))
                
-        text=[w for w in text if not w in stopw and len(w)>3]
+        #print(text)
+         
+        words = nltk.word_tokenize(text)
+        functors_pos = {'CONJ', 'ADV-PRO', 'CONJ', 'PART'}  # function words
 
+        text=[word for word, pos in nltk.pos_tag(words, lang='rus')if pos not in functors_pos]
+        #print(text)
+         
+        text=[w for w in text if not w in stopw and len(w)>3]  
 
         text=LemmatizeMorphText(text)
         
-    
         text=Steemm(text)
-    
-        text=' '.join(text)
+        
+        token=' '.join(text)
         #print(text)
         toc = time.perf_counter()
         print(f"Вычисление заняло {toc - tic:0.4f} секунд")
-    return text
-    
+        
+    return token
+
+
+
     
