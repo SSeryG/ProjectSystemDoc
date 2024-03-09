71e6892bd9fffeb2d2ef8bb97ed5d59f0fca7c4b
SseryG <emanaev2000@mail.ru>
2024-03-10 01:48:11 +0700

    read and write data

--- clusters.py
+++ clusters.py
@@ -7,23 +7,42 @@ from nltk import ngrams
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+import time
 
 class ClustersClass(object):
     def __init__(self):
-        self.model=AgglomerativeClustering(distance_threshold=1.5,n_clusters=None)        
-        self.data=pd.DataFrame()
+        self.model=AgglomerativeClustering(distance_threshold=1.4,n_clusters=None)        
+        
+        
+    def IsDataCSV(self,data):
+        tic = time.perf_counter()
+        if os.path.isfile('data.csv'):
+            datacsv=pd.read_csv('data.csv',sep='\t',index_col=0)
+            toc = time.perf_counter()
+            print(f"Вычисление заняло {toc - tic:0.4f} секунд")
+            data=pd.concat([data, datacsv])
+            data=data.fillna(0)   
+        data.to_csv('data.csv',sep='\t')
+        toc = time.perf_counter()
+        print(f"Вычисление заняло {toc - tic:0.4f} секунд")
+        return data
       
     def OneCluster(self,X):            
-        self.data=pd.concat([self.data, X])
-        labels=self.model.fit_predict(self.data)    
+        data=self.IsDataCSV(X)        
+           
+        print(data)        
+        data.drop_duplicates(keep='last')
+        
+        print(data)
+        labels=self.model.fit_predict(data)    
         print(labels)
         return labels
     
-def TFidfVector(artext):#построение и вычисление частотности
+def TFidfVector(artext,index):#построение и вычисление частотности
         tfIdfVectorizer = TfidfVectorizer(ngram_range=(2,3),use_idf = True)
         tfIdf = tfIdfVectorizer.fit_transform(artext)
         tokens=(tfIdfVectorizer.get_feature_names_out())
-        df_tfidf = pd.DataFrame(tfIdf.toarray(), columns = tokens)
-        print(df_tfidf)        
+        df_tfidf = pd.DataFrame(tfIdf.toarray(),index, columns = tokens)           
         return df_tfidf
 
